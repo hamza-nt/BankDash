ced89d59b62a6f7069c9515b5b78c5fdbb5d09c9
hamza-nejib <hamza-nejib@outlook.com>
2024-11-15 17:46:08 +0100

    Début de Projet BankDash

---
+++ Backend-Spring/src/main/java/com/bankdash/BankDashApplication.java
@@ -0,0 +1,13 @@
+package com.bankdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankDashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankDashApplication.class, args);
+	}
+
+}
---
+++ Backend-Spring/src/test/java/com/bankdash/BankDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
